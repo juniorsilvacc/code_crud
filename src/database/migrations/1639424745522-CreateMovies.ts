67135670b01e19ff0e957863aa607f882be788f0
juniorsilvacc <juniorsilvacc@hotmail.com>
2021-12-13 17:45:49 -0300

    created migration movies

---
+++ src/database/migrations/1639424745522-CreateMovies.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateMovies1639424745522 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'movies',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'duration',
+            type: 'numeric',
+          },
+          {
+            name: 'category_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'FK_Movies_Category',
+            columnNames: ['category_id'],
+            referencedTableName: 'categories',
+            referencedColumnNames: ['id'],
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('movies');
+  }
+}
