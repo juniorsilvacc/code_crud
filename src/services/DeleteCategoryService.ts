bbdd4d70b17ebecba502385272d4c1b403621b35
juniorsilvacc <juniorsilvacc@hotmail.com>
2021-12-13 23:02:32 -0300

    removing category

---
+++ src/services/DeleteCategoryService.ts
@@ -0,0 +1,17 @@
+import { getRepository } from 'typeorm';
+
+import { Category } from '../entities/Category';
+
+class DeleteCategoryService {
+  async execute(id: string) {
+    const repo = getRepository(Category);
+
+    if (!(await repo.findOne(id))) {
+      return new Error('Category doest not exists');
+    }
+
+    await repo.delete(id);
+  }
+}
+
+export { DeleteCategoryService };
