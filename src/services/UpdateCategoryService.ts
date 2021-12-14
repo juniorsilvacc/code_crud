f4d3c17d76076493f3ec4ef706deb3cf45a13ba5
juniorsilvacc <juniorsilvacc@hotmail.com>
2021-12-13 23:35:48 -0300

    updating category data

---
+++ src/services/UpdateCategoryService.ts
@@ -0,0 +1,30 @@
+import { getRepository } from 'typeorm';
+
+import { Category } from '../entities/Category';
+
+interface IRequest {
+  id: string;
+  name: string;
+  description: string;
+}
+
+class UpdateCategoryService {
+  async execute({ id, name, description }: IRequest) {
+    const repo = getRepository(Category);
+
+    const category = await repo.findOne(id);
+
+    if (!category) {
+      return new Error('Category doest not exists');
+    }
+
+    category.name = name || category.name;
+    category.description = description || category.description;
+
+    await repo.save(category);
+
+    return category;
+  }
+}
+
+export { UpdateCategoryService };
