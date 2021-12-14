191cecccfdc8c574f9fbb6fdeed5ed74aae6d4ff
juniorsilvacc <juniorsilvacc@hotmail.com>
2021-12-13 21:36:39 -0300

    create category service

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,29 @@
+import { getRepository } from 'typeorm';
+
+import { Category } from '../entities/Category';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateCategoryService {
+  async execute({ name, description }: IRequest): Promise<Category | Error> {
+    const repo = getRepository(Category);
+
+    if (await repo.findOne({ name })) {
+      return new Error('Category already exists');
+    }
+
+    const category = repo.create({
+      name,
+      description,
+    });
+
+    await repo.save(category);
+
+    return category;
+  }
+}
+
+export { CreateCategoryService };
